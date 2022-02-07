8b4b1039d2c03ffe0d26499f03552407e9180170
tetris.dev.web <tetris.dev.web@gmail.com>
2022-02-08 04:56:08 +0900

    update user add dialog

--- src/components/UserDialog/index.tsx
+++ src/components/UserDialog/index.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
-import { Modal, Button, TextField, Box } from "@material-ui/core";
+import {
+  Modal,
+  Button,
+  TextField,
+  Box,
+  FormControlLabel,
+  Checkbox,
+} from "@material-ui/core";
 import { v4 as uuid } from "uuid";
-import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { useFormik, Form, FormikProvider } from "formik";
+import { useForm, Controller } from "react-hook-form";
 // import { useForm } from '../../hooks/useForm';
 import { makeStyles } from "@material-ui/core/styles";
+// import { LoadingButton } from "@material-ui/lab";
 import { useStores } from "../../use-stores";
 
 const useStyles = makeStyles({
@@ -30,79 +40,101 @@ export default function UserDialog({ isOpen, closeModal }: IProps) {
     // handleUser({e.target.name: e.target.value})
   };
 
-  const [username, setUsername] = useState("");
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
   const [enabled, setEnabled] = useState(false);
-  // const [user, handleUser] = useForm({ id, username, firstname, lastname });
-  // const [user, handleUser] = useForm({});
 
-  // const handleSubmit = (e: React.FormEvent) => {
-  //   e.preventDefault();
-  //   const newUser = {
-  //     id: Date.now(),
-  //     username: username,
-  //     firstname: firstname,
-  //     lastname: lastname,
-  //     enabled: enabled,
-  //   };
-  //   console.log(newUser);
-  //   userStore.addUser(newUser);
-  //   closeModal();
-  // };
+  const ValidationSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("User Name required"),
+    firstname: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("First Name required"),
+    lastname: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Last Name required"),
+  });
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data: any) => {
-    console.log(data);
-  };
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      firstname: "",
+      lastname: "",
+    },
+    validationSchema: ValidationSchema,
+    onSubmit: async (values, { setErrors, setSubmitting, resetForm }) => {
+      console.log("VALUE", values);
+      const newUser = {
+        id: Date.now(),
+        username: values.username,
+        firstname: values.firstname,
+        lastname: values.lastname,
+        enabled: enabled,
+      };
+      setTimeout(() => {
+        userStore.addUser(newUser);
+        closeModal();
+      }, 1000);
+      
+    },
+  });
+
+  const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik;
 
   return (
     <Modal aria-labelledby="modal-new-todo" open={isOpen} onClose={closeModal}>
       <Box className={classes.content}>
         <h2 id="modal-new-todo">Add User</h2>
-        <form onSubmit={onSubmit} noValidate>
-          <TextField
-            id="standard-basic"
-            autoFocus
-            label="Username"
-            fullWidth
-            // onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            //   setUsername(e.target.value)
-            // }
-            style={{ marginBottom: "1rem" }}
-          />
-          <TextField
-            id="standard-basic"
-            label="Firstname"
-            fullWidth
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setFirstname(e.target.value)
-            }
-            style={{ marginBottom: "1rem" }}
-          />
-          <TextField
-            id="standard-basic"
-            label="Lastname"
-            fullWidth
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setLastname(e.target.value)
-            }
-            style={{ marginBottom: "1rem" }}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            style={{ marginTop: "1rem" }}
-          >
-            Submit
-          </Button>
-        </form>
+        <FormikProvider value={formik}>
+          <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              label="User Name"
+              {...getFieldProps("username")}
+              error={Boolean(touched.username && errors.username)}
+              helperText={touched.username && errors.username}
+              style={{ marginBottom: "1rem" }}
+            />
+            <TextField
+              fullWidth
+              label="First Name"
+              {...getFieldProps("firstname")}
+              error={Boolean(touched.firstname && errors.firstname)}
+              helperText={touched.firstname && errors.firstname}
+              style={{ marginBottom: "1rem" }}
+            />
+            <TextField
+              fullWidth
+              label="Last Name"
+              {...getFieldProps("lastname")}
+              error={Boolean(touched.lastname && errors.lastname)}
+              helperText={touched.lastname && errors.lastname}
+              style={{ marginBottom: "1rem" }}
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  // {...enabled ? "checked" : ""}
+                  onChange={() => setEnabled(!enabled)}
+                  color="primary"
+                />
+              }
+              label="Enabled"
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              style={{ marginTop: "1rem" }}
+            >
+              Submit
+            </Button>
+          </Form>
+        </FormikProvider>
       </Box>
     </Modal>
   );
